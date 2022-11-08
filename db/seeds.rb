15765d0df4537c2201939950024fb584144422bd
Clement Choi <clementchoi1997@gmail.com>
2022-11-08 16:31:38 +0900

    CRUD implemented

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+require 'faker'
+
+10.times do
+  Article.create(title: Faker::Lorem.sentence, content: Faker::Lorem.paragraph(sentence_count: 35, supplemental: true, random_sentences_to_add: 5))
+end
